982e3b9fccdb21bbef4410432b58e9c4e12ee30e
unknown <paugalk1@gmail.com>
2018-11-17 22:31:21 +0100

    add

--- front_end/src/app/homepage/homepage-routing.module.ts
+++ front_end/src/app/homepage/homepage-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EntranceComponent } from './entrance/entrance.component';
+import { AddGroupComponent } from './add-group/add-group.component';
 
 const routes: Routes = [
   {
     path: 'entrance',
     component: EntranceComponent
+  },
+  {
+    path: 'add',
+    component: AddGroupComponent
   }
 ];
 
