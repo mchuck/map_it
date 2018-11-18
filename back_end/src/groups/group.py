d7f1e5d03db8b9eb676a021160d67efa32696605
mchuck <msz93@o2.pl>
2018-11-18 11:22:04 +0100

    backend_update

--- back_end/src/groups/group.py
+++ back_end/src/groups/group.py
@@ -32,10 +32,14 @@ class Group(object):
  
     def add_participant(self, participant):
         if participant.name in self.participants:
-            raise Exception('Participant with the name: {} already exists'\
+            raise Exception('Participant with the name: {} already exists' \
                             .format(participant.name))
         else:
-            self.participants[participant.name] = participant
+            if participant.type == 'guide' and \
+               not all([p.type=='normal' for p in self.participants.values()]):
+                raise Exception('There can only be one guide per room')
+            else:                
+                self.participants[participant.name] = participant
 
     def delete_participant(self, participant_name):
         if participant_name in self.participants:
