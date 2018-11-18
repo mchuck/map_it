34166d5fff925babd1c9af3f7e1b4229ad47d87d
mchuck <msz93@o2.pl>
2018-11-18 11:55:41 +0100

    group view error handling

--- front_end/src/app/homepage/group-view/group-view.component.ts
+++ front_end/src/app/homepage/group-view/group-view.component.ts
@@ -4,41 +4,45 @@ import { HomepageService } from '../homepage.service';
 import { MapViewComponent } from 'src/app/map/map-view/map-view.component';
 
 @Component({
-  selector: 'app-group-view',
-  templateUrl: './group-view.component.html',
-  styleUrls: ['./group-view.component.css']
+    selector: 'app-group-view',
+    templateUrl: './group-view.component.html',
+    styleUrls: ['./group-view.component.css']
 })
 export class GroupViewComponent implements OnInit {
 
-  groupKey: string;
+    groupKey: string;
 
-  @ViewChild('mapView') mapView: MapViewComponent;
+    @ViewChild('mapView') mapView: MapViewComponent;
 
-  constructor(private route: ActivatedRoute, private hostService: HomepageService, private router: Router) {
-    this.groupKey = this.route.snapshot.paramMap.get('name');
-  }
+    constructor(private route: ActivatedRoute, private hostService: HomepageService, private router: Router) {
+        this.groupKey = this.route.snapshot.paramMap.get('name');
+    }
 
-  userName = '';
-  userType: 'normal' | 'guide' = 'normal';
+    userName = '';
+    userType: 'normal' | 'guide' = 'normal';
+    messageError: string;
+    userInserted = false;
 
-  userInserted = false;
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    createUser() {
+        this.hostService.joinToGroup({ name: this.userName, type: this.userType }, this.groupKey).subscribe(res => {
+            this.userInserted = true;
+        }, error => {
+            console.log(error);
+            this.messageError = error.error || 'Unknown error';
+            setTimeout(() => this.messageError = null, 3000);
+        });
+    }
 
-  createUser() {
-    this.hostService.joinToGroup({ name: this.userName, type: this.userType }, this.groupKey).subscribe(res => {
-      this.userInserted = true;
-    }, error => alert(error.message || error));
-  }
+    setType(type: 'normal' | 'guide') {
+        this.userType = type;
+    }
 
-  setType(type: 'normal' | 'guide') {
-    this.userType = type;
-  }
-
-  onUnsub() {
-    this.mapView.unsubscribeUser(this.groupKey, this.userName).subscribe(_ => {
-      this.router.navigate(['/']);
-    });
-  }
+    onUnsub() {
+        this.mapView.unsubscribeUser(this.groupKey, this.userName).subscribe(_ => {
+            this.router.navigate(['/']);
+        });
+    }
 }
