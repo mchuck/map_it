ab3c5cf26815992035f76515c59d1b04832df574
Tomasz Krzywonos <tomekrzywonos@gmail.com>
2018-11-18 09:50:12 +0100

    Add wachPosition

--- front_end/src/app/map/map-view/map-view.component.ts
+++ front_end/src/app/map/map-view/map-view.component.ts
@@ -13,118 +13,123 @@ const iconNormal = divIcon({ className: 'div-icon normal' });
 const iconGuide = divIcon({ className: 'div-icon guide' });
 
 const iconSVG = icon({
-    iconUrl: 'assets/images/marker.svg',
-    iconSize: [50, 50]
+  iconUrl: 'assets/images/marker.svg',
+  iconSize: [50, 50]
 });
 
 const iconSVGGuide = icon({
-    iconUrl: 'assets/images/marker-guide.svg',
-    iconSize: [50, 50]
+  iconUrl: 'assets/images/marker-guide.svg',
+  iconSize: [50, 50]
 });
 
 @Component({
-    selector: 'app-map-view',
-    templateUrl: './map-view.component.html',
-    styleUrls: ['./map-view.component.css']
+  selector: 'app-map-view',
+  templateUrl: './map-view.component.html',
+  styleUrls: ['./map-view.component.css']
 })
 export class MapViewComponent implements OnInit, OnDestroy {
 
-    @Input() groupName: string;
-    @Input() userName: string;
-    @Input() userType: 'guide' | 'normal';
+  @Input() groupName: string;
+  @Input() userName: string;
+  @Input() userType: 'guide' | 'normal';
 
-    @ViewChild('mapContainer') mapContainer: ElementRef;
+  @ViewChild('mapContainer') mapContainer: ElementRef;
 
-    mapL: Map = undefined;
+  mapL: Map = undefined;
 
-    markers: Marker<any>[] = [];
+  markers: Marker<any>[] = [];
 
-    markersLayer: LayerGroup<any>;
+  markersLayer: LayerGroup<any>;
 
-    leyer: any;
+  leyer: any;
 
 
-    private localizationHandler: any;
-    private isLoading = false;
+  private localizationHandler: any;
+  private isLoading = false;
 
-    constructor(private mapService: MapService, private cred: MapCredentials, private locService: LocalizationService) { }
+  constructor(private mapService: MapService, private cred: MapCredentials, private locService: LocalizationService) { }
 
-    ngOnInit() {
-        this.initMap();
-        this.localizationHandler = this.locService.getLocalization(
-            (p) => this.onGetLocalizationSuccess(p),
-            error => console.log(error));
+  ngOnInit() {
+    this.initMap();
+
+  }
+
+  initMap() {
+    this.mapL = map(this.mapContainer.nativeElement)
+    tileLayer(`https://1.base.maps.api.here.com/maptile/2.1/maptile/newest/` +
+      `reduced.day/{z}/{x}/{y}/256/png8?app_id=pYcVUdzXaKUNelaYX98n&app_code=e4Nq7y32dS96gUbBFbNllg`)
+      .addTo(this.mapL);
+
+    this.locService.getLocalizatino((p) => this.onGetLocalizationSuccess(p),
+      error => console.log(error));
+
+    this.localizationHandler = this.locService.watchPosition(
+      (p) => this.onGetLocalizationSuccess(p),
+      error => console.log(error));
+  }
+
+  getIcon(type: 'guide' | 'normal') {
+    return type === 'guide' ? iconGuide : iconNormal;
+  }
+
+  getIconSVG(type: 'guide' | 'normal') {
+    return type === 'guide' ? iconSVGGuide : iconSVG;
+  }
+
+  onGetLocalizationSuccess(position: Position) {
+    console.log('inites')
+    if (this.isLoading) {
+      return;
     }
+    console.log('exited')
+    this.isLoading = true;
 
-    initMap() {
-        this.mapL = map(this.mapContainer.nativeElement)
-        tileLayer(`https://1.base.maps.api.here.com/maptile/2.1/maptile/newest/` +
-            `reduced.day/{z}/{x}/{y}/256/png8?app_id=pYcVUdzXaKUNelaYX98n&app_code=e4Nq7y32dS96gUbBFbNllg`)
-            .addTo(this.mapL);
-    }
-
-    getIcon(type: 'guide' | 'normal') {
-        return type === 'guide' ? iconGuide : iconNormal;
-    }
-
-    getIconSVG(type: 'guide' | 'normal') {
-        return type === 'guide' ? iconSVGGuide : iconSVG;
-    }
-
-    onGetLocalizationSuccess(position: Position) {
-        console.log('inites')
-        if (this.isLoading) {
-            return;
+    this.mapService.updateLocalization(this.groupName, this.userName, position.coords as Coord)
+      .subscribe(res => {
+        this.updateMarkers(res.participants)
+        // this.getIcon(user.type)
+        if (this.markersLayer) {
+          console.log('removed')
+          this.mapL.removeLayer(this.markersLayer);
         }
-        console.log('exited')
-        this.isLoading = true;
 
-        this.mapService.updateLocalization(this.groupName, this.userName, position.coords as Coord)
-            .subscribe(res => {
-                this.updateMarkers(res.participants)
-                // this.getIcon(user.type)
-                if (this.markersLayer) {
-                    console.log('removed')
-                    this.mapL.removeLayer(this.markersLayer);
-                }
-
-                if (this.markers) {
-                    this.markersLayer = layerGroup(this.markers).addTo(this.mapL);
-                    this.suitMapToMarkers();
-                }
-                this.isLoading = false;
-            });
-    }
-
-    updateMarkers(participants: any) {
-        this.markers = [];
-        for (const user of participants) {
-            const userPosition = user.positions.slice(-1)[0];
-            if (!userPosition) {
-                continue;
-            }
-            this.markers.push(marker([userPosition.lat, userPosition.lon],
-                { icon: this.getIconSVG(user.type) })
-                .bindPopup(this.createMarkerPopupContent(user)));
+        if (this.markers) {
+          this.markersLayer = layerGroup(this.markers).addTo(this.mapL);
+          this.suitMapToMarkers();
         }
-    }
+        this.isLoading = false;
+      }, error => this.isLoading = false);
+  }
 
-    unsubscribeUser(groupKey: string, userName: string) {
-        return this.mapService.unsubscribeFromGroup(groupKey, userName);
+  updateMarkers(participants: any) {
+    this.markers = [];
+    for (const user of participants) {
+      const userPosition = user.positions.slice(-1)[0];
+      if (!userPosition) {
+        continue;
+      }
+      this.markers.push(marker([userPosition.lat, userPosition.lon],
+        { icon: this.getIconSVG(user.type) })
+        .bindPopup(this.createMarkerPopupContent(user)));
     }
+  }
 
-    suitMapToMarkers() {
-        const group = featureGroup(this.markers);
+  unsubscribeUser(groupKey: string, userName: string) {
+    return this.mapService.unsubscribeFromGroup(groupKey, userName);
+  }
 
-        this.mapL.fitBounds(group.getBounds());
-    }
+  suitMapToMarkers() {
+    const group = featureGroup(this.markers);
 
-    createMarkerPopupContent(user) {
-        return `<b>${user.name}</b>`;
-    }
+    this.mapL.fitBounds(group.getBounds());
+  }
 
-    ngOnDestroy(): void {
-        this.unsubscribeUser(this.groupName, this.userName);
-        this.locService.stopLocaliztion(this.localizationHandler);
-    }
+  createMarkerPopupContent(user) {
+    return `<b>${user.name}</b>`;
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeUser(this.groupName, this.userName);
+    this.locService.stopLocaliztion(this.localizationHandler);
+  }
 }
