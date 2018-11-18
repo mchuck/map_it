5f5786a5d085e172cba0d50ec25aa8f1948a3617
mchuck <msz93@o2.pl>
2018-11-18 04:39:55 +0100

    fix

--- front_end/src/app/homepage/entrance/entrance.component.ts
+++ front_end/src/app/homepage/entrance/entrance.component.ts
@@ -4,28 +4,29 @@ import { Router } from '@angular/router';
 import { GroupInformation } from '../models/new-group';
 
 @Component({
-  selector: 'app-entrance',
-  templateUrl: './entrance.component.html',
-  styleUrls: ['./entrance.component.css']
+    selector: 'app-entrance',
+    templateUrl: './entrance.component.html',
+    styleUrls: ['./entrance.component.css']
 })
 export class EntranceComponent implements OnInit {
 
-  constructor(
-    private homeService: HomepageService,
-    private router: Router,
-    private host: HomepageService) { }
+    constructor(
+        private homeService: HomepageService,
+        private router: Router,
+        private host: HomepageService) { }
 
 
-    private messageError: string;
-  ngOnInit() {
-  }
+    messageError: string;
+    ngOnInit() {
+    }
 
-  onJoinGroup(value: string) {
-    this.homeService.getGroup(value).subscribe((name: GroupInformation) => {
-      this.router.navigate(['/groupview', value]);
-    }, error => {
-      this.messageError = error.error || "Unknown error";
-      setTimeout(()=> this.messageError=null, 3000)
-    });
-}
+    onJoinGroup(value: string) {
+        this.homeService.getGroup(value).subscribe((name: GroupInformation) => {
+            this.router.navigate(['/groupview', value]);
+        }, error => {
+            console.log(error);
+            this.messageError = error.error || "Unknown error";
+            setTimeout(() => this.messageError = null, 3000)
+        });
+    }
 }
